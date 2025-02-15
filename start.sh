5ae66f01a015178f1e4970f66310edf1e5625b3c
Ronan-Yann Lorin <rylorin@gmail.com>
2025-02-15 17:30:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #set -x
+set -e  # Exit immediately if a command exits with a non-zero status
 
 if [ -n "${GIT_URL-}" ]; then
-	echo "Fetching ${GIT_URL}"
-	git clone ${GIT_URL} streamlit_app
+	echo "Fetching ${GIT_URL}..."
+	git clone --depth=1 ${GIT_URL} streamlit_app
 fi
 
-cd streamlit_app
+cd streamlit_app || { echo "Directory 'streamlit_app' not found!"; exit 1; }
 
-echo "Installing packages"
-pip3 install -r requirements.txt >/dev/null
+echo "Installing dependencies..."
+pip3 --root-user-action install -r requirements.txt >/dev/null
 
-echo "Running app"
-streamlit run streamlit_app.py
+echo "Starting..."
+exec streamlit run streamlit_app.py
